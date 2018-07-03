759614b329caa68daff1671b709b37ee5b70d673
nightsense <nightsense@users.noreply.github.com>
2018-07-03 09:30:57 -0400

    Revert "clean up gen script formatting"
    This reverts commit 7f9ae0abd4c9ba9a5cc9972467c6f4310adc3f2c.

--- templates/template-generator.py
+++ templates/template-generator.py
@@ -6,27 +6,33 @@ def tohex(l,c,h):
   clamp = sRGBColor(rgb.clamped_rgb_r, rgb.clamped_rgb_g, rgb.clamped_rgb_b)
   return clamp.get_rgb_hex()
 
-hexgry0_lt = tohex( 12.00/12.0*100.0 , 00.00/12.0*100 , 255.00 )
-hexgry1_lt = tohex( 11.25/12.0*100.0 , 00.25/12.0*100 , 255.00 )
-hexgry2_lt = tohex( 05.98/12.0*100.0 , 01.25/12.0*100 , 255.00 )
-hexgry3_lt = tohex( 03.69/12.0*100.0 , 01.50/12.0*100 , 255.00 )
+hexgry0_lt = tohex( 11.75/12.0*100.0 , 00.25/12.0*100 , 258.75 )
+hexgry1_lt = tohex( 11.00/12.0*100.0 , 00.50/12.0*100 , 258.75 )
+hexgry2_lt = tohex( 07.00/12.0*100.0 , 01.25/12.0*100 , 258.75 )
+hexgry3_lt = tohex( 05.50/12.0*100.0 , 01.50/12.0*100 , 258.75 )
 
-hexred__lt = tohex( 06.00/12.0*100.0 , 12.00/12.0*100 , 037.50 )
+hexred__lt = tohex( 06.75/12.0*100.0 , 08.00/12.0*100 , 033.75 )
+hexorng_lt = tohex( 07.25/12.0*100.0 , 07.50/12.0*100 , 063.75 )
 hexyllw_lt = tohex( 09.50/12.0*100.0 , 12.00/12.0*100 , 086.25 )
-hexgren_lt = tohex( 05.60/12.0*100.0 , 12.00/12.0*100 , 150.00 )
-hexblue_lt = tohex( 06.00/12.0*100.0 , 12.00/12.0*100 , 296.25 )
-hexprpl_lt = tohex( 06.00/12.0*100.0 , 12.00/12.0*100 , 330.00 )
+hexgren_lt = tohex( 07.00/12.0*100.0 , 07.00/12.0*100 , 123.75 )
+hexaqua_lt = tohex( 07.00/12.0*100.0 , 05.50/12.0*100 , 168.75 )
+hexblue_lt = tohex( 06.75/12.0*100.0 , 05.50/12.0*100 , 258.75 )
+hexprpl_lt = tohex( 06.50/12.0*100.0 , 05.50/12.0*100 , 296.25 )
+hexpink_lt = tohex( 07.25/12.0*100.0 , 05.50/12.0*100 , 003.75 )
 
-hexgry0_dk = tohex( 02.00/12.0*100.0 , 01.00/12.0*100 , 255.00 )
-hexgry1_dk = tohex( 02.75/12.0*100.0 , 01.25/12.0*100 , 255.00 )
-hexgry2_dk = tohex( 06.00/12.0*100.0 , 01.50/12.0*100 , 255.00 )
-hexgry3_dk = tohex( 07.50/12.0*100.0 , 01.50/12.0*100 , 255.00 )
+hexgry0_dk = tohex( 02.00/12.0*100.0 , 01.00/12.0*100 , 258.75 )
+hexgry1_dk = tohex( 02.75/12.0*100.0 , 01.25/12.0*100 , 258.75 )
+hexgry2_dk = tohex( 06.00/12.0*100.0 , 01.50/12.0*100 , 258.75 )
+hexgry3_dk = tohex( 07.50/12.0*100.0 , 01.50/12.0*100 , 258.75 )
 
 hexred__dk = tohex( 06.75/12.0*100.0 , 03.50/12.0*100 , 033.75 )
+hexorng_dk = tohex( 06.75/12.0*100.0 , 03.25/12.0*100 , 063.75 )
 hexyllw_dk = tohex( 09.50/12.0*100.0 , 06.00/12.0*100 , 086.25 )
-hexgren_dk = tohex( 06.75/12.0*100.0 , 02.50/12.0*100 , 157.50 )
-hexblue_dk = tohex( 06.75/12.0*100.0 , 03.00/12.0*100 , 255.00 )
-hexprpl_dk = tohex( 06.75/12.0*100.0 , 03.00/12.0*100 , 255.00 )
+hexgren_dk = tohex( 06.75/12.0*100.0 , 03.00/12.0*100 , 123.75 )
+hexaqua_dk = tohex( 06.75/12.0*100.0 , 02.50/12.0*100 , 168.75 )
+hexblue_dk = tohex( 06.75/12.0*100.0 , 03.00/12.0*100 , 258.75 )
+hexprpl_dk = tohex( 06.75/12.0*100.0 , 03.00/12.0*100 , 296.25 )
+hexpink_dk = tohex( 06.75/12.0*100.0 , 02.25/12.0*100 , 003.75 )
 
 print '# general'
 print 'Author:          nightsense'
@@ -44,13 +50,14 @@ print 'Color:      gry2 %s ~' % hexgry2_lt
 print 'Color:      gry3 %s ~' % hexgry3_lt
 print 'Color:      gry4 %s ~' % hexgry0_dk
 print 'Color:      red_ %s ~' % hexred__lt
+print 'Color:      orng %s ~' % hexorng_lt
 print 'Color:      yllw %s ~' % hexyllw_lt
 print 'Color:      gren %s ~' % hexgren_lt
+print 'Color:      aqua %s ~' % hexaqua_lt
 print 'Color:      blue %s ~' % hexblue_lt
 print 'Color:      prpl %s ~' % hexprpl_lt
+print 'Color:      pink %s ~' % hexpink_lt
 print 'Include:    _common.colortemplate'
-print 'DiffChanged gry4 yllw'
-print 'DiffText    gry4 yllw'
 print 'MatchParen  gry4 yllw'
 print 'Search      gry4 yllw'
 print ''
@@ -61,12 +68,13 @@ print 'Color:      gry1 %s ~' % hexgry1_dk
 print 'Color:      gry2 %s ~' % hexgry2_dk
 print 'Color:      gry3 %s ~' % hexgry3_dk
 print 'Color:      red_ %s ~' % hexred__dk
+print 'Color:      orng %s ~' % hexorng_dk
 print 'Color:      yllw %s ~' % hexyllw_dk
 print 'Color:      gren %s ~' % hexgren_dk
+print 'Color:      aqua %s ~' % hexaqua_dk
 print 'Color:      blue %s ~' % hexblue_dk
 print 'Color:      prpl %s ~' % hexprpl_dk
+print 'Color:      pink %s ~' % hexpink_dk
 print 'Include:    _common.colortemplate'
-print 'DiffChanged gry0 yllw'
-print 'DiffText    gry0 yllw'
 print 'MatchParen  gry0 yllw'
 print 'Search      gry0 yllw'
