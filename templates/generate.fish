dfac4f834deb166e303ef2827582ceee6fe42f0a
nightsense <nightsense@users.noreply.github.com>
2018-07-06 14:19:35 -0400

    shorten quit command in gen script

--- templates/generate.fish
+++ templates/generate.fish
@@ -2,7 +2,6 @@ for s in \
   snow_redshift_plus snow_redshift snow snow_blueshift snow_blueshift_plus
   set t $s'.colortemplate'
   python template-generator.py $s > $t
-  vim -c 'Colortemplate! ~/Documents/code/vim/snow' $t \
-    -c 'q!' -c 'q!' -c 'q!' -c 'q!' -c 'q!' -c 'q!' -c 'q!'
+  vim -c 'Colortemplate! ~/Documents/code/vim/snow' $t -c 'qa!'
   sed -i '/Last Updated/d' ../colors/$s.vim
 end
