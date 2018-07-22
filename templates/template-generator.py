a820d6dd9c490ae8793858c30349f8851e03e05f
nightsense <nightsense@users.noreply.github.com>
2018-07-22 05:42:47 -0400

    nudge base colors for contrast ratios

--- templates/template-generator.py
+++ templates/template-generator.py
@@ -95,9 +95,9 @@ hue_base = 262.50
 hexgry0_lt = tohex( gold(0.10) , gold(5.00) , hue_base )
 hexgry1_lt = tohex( gold(0.23) , gold(4.50) , hue_base )
 hexgry2_lt = tohex( gold(1.30) , gold(4.00) , hue_base )
-hexgry3_lt = tohex( gold(2.04) , gold(4.00) , hue_base )
+hexgry3_lt = tohex( gold(2.03) , gold(4.00) , hue_base )
 
-hexgry0_dk = tohex( gold(3.55) , gold(5.00) , hue_base )
+hexgry0_dk = tohex( gold(3.54) , gold(5.00) , hue_base )
 hexgry1_dk = tohex( gold(3.10) , gold(4.50) , hue_base )
 hexgry2_dk = tohex( gold(1.30) , gold(4.00) , hue_base )
 hexgry3_dk = tohex( gold(0.71) , gold(4.00) , hue_base )
